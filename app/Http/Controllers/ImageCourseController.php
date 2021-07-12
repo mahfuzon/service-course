35e9cd9d88e3162c65adf0039b4a8805bbabe491
Mahfuzon Akhiar <mahfuzon0@gmail.com>
2021-07-12 21:49:32 +0700

    setting migration dan db

---
+++ app/Http/Controllers/ImageCourseController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ImageCourse;
+use Illuminate\Http\Request;
+
+class ImageCourseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ImageCourse  $imageCourse
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ImageCourse $imageCourse)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ImageCourse  $imageCourse
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ImageCourse $imageCourse)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ImageCourse  $imageCourse
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ImageCourse $imageCourse)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ImageCourse  $imageCourse
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ImageCourse $imageCourse)
+    {
+        //
+    }
+}
